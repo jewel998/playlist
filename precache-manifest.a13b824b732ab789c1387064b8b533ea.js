150f13f10db3050814e68b2595fdf0f6510c64b5
Jyotirmoy Barman <31440034+jewel998@users.noreply.github.com>
2020-09-13 16:02:14 +0530

    New Version
    Done many Bug Fixes

---
+++ precache-manifest.a13b824b732ab789c1387064b8b533ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d004b53a4e4852eea3064909bf37c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7bc26a567c9dc3f055",
+    "url": "/static/css/main.e869ca1f.chunk.css"
+  },
+  {
+    "revision": "d0c65c19383b0763ec07",
+    "url": "/static/js/2.9d7987a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d7987a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7bc26a567c9dc3f055",
+    "url": "/static/js/main.1c6f00d9.chunk.js"
+  },
+  {
+    "revision": "a3c8ef02e0e11ef70e9e",
+    "url": "/static/js/runtime-main.a477f6c7.js"
+  }
+]);
