3d0f6191ba4856caaf105b9ba2f38cd96aedfd1d
Jyotirmoy Barman <jyotirmoy.barman@bluestacks.com>
2020-09-13 16:49:26 +0530

    New version

---
+++ precache-manifest.5bf8b30e6835614fceef32e059fe7ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a515aa924c0a1b8c868365710cf496d2",
+    "url": "/playlist/index.html"
+  },
+  {
+    "revision": "7dc82c1106c218cd01bd",
+    "url": "/playlist/static/css/main.e869ca1f.chunk.css"
+  },
+  {
+    "revision": "d0c65c19383b0763ec07",
+    "url": "/playlist/static/js/2.9d7987a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/playlist/static/js/2.9d7987a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc82c1106c218cd01bd",
+    "url": "/playlist/static/js/main.99b16082.chunk.js"
+  },
+  {
+    "revision": "4098689072fc3be9a5c3",
+    "url": "/playlist/static/js/runtime-main.53bc9397.js"
+  }
+]);
